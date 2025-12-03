736d72dc4dfc11796a08d04325b6a5767327bb9b
lee-min-seo-official <subscribe.evato.elements@gmail.com>
2025-12-03 14:36:39 +0500

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
+module.exports = {
+  PREFIX: process.env.PREFIX || "IRON-M4N×",
+  EMAIL: process.env.EMAIL || "subscribe.evato.elements@gmail.com",
+  PASS: process.env.PASS || "Nimra321#",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "http://cdn.shoob.gg/images/cards/4/62dcae006357acae8ea165c7007345e6766a055cdbe46aa6045e114f2c99b05b.png",
+}
