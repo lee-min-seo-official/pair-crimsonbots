f1bfe45d6004d7e43fcf87df7f375b6a6b939350
lee-min-seo-official <subscribe.evato.elements@gmail.com>
2025-12-04 00:59:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
   PREFIX: process.env.PREFIX || "IRON-M4N×",
   EMAIL: process.env.EMAIL || "subscribe.evato.elements@gmail.com",
-  PASS: process.env.PASS || "Nimra321#",
+  PASS: process.env.PASS || "Nimsss321#",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
   IMAGE: process.env.IMAGE || "http://cdn.shoob.gg/images/cards/4/62dcae006357acae8ea165c7007345e6766a055cdbe46aa6045e114f2c99b05b.png",
 }
