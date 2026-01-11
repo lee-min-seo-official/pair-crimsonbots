9d27e22c97d10552b92b200633af98e00ffdc25f
lee-min-seo-official <subscribe.evato.elements@gmail.com>
2026-01-12 00:24:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function connector(Num, res) {
         },
       //  printQRInTerminal: false,
         logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
-        browser: Browsers.mac("Safari"), //check docs for more custom options
+        browser: Browsers.macOS("Safari"), //check docs for more custom options
         markOnlineOnConnect: true, //true or false yoour choice
         msgRetryCounterCache
     });
